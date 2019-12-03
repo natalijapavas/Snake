0668df52c18efd2453aa8f29d8bbdabf25c44c82
Natalija <natalijapavasovic@gmail.com>
2019-12-03 19:38:56 +0100

    dodatak

--- game.py
+++ game.py
@@ -1,5 +1,47 @@
 #!/usr/bin/env python3
 # snake
-import pyautogui
-print("Welcome to snake game")
-print("dodatak")
+from typing import Dict, Union
+import random
+from flask import Flask
+
+import json
+
+moves = ["up", "down", "left", "right"]
+
+Snake = {}
+
+app = Flask(__name__)
+
+@app.route("/start", methods=["POST"])
+def start(req):
+    move = {
+    "move": random.choice(moves),
+    "taunt": "r4ndom"
+    }
+    move_json = json.dumps(move)
+    print(req)
+    return move_json
+
+@app.route("/move", methods=["POST"])
+def move(req):
+    move = {
+    "move": random.choice(moves),
+    "taunt": "r4ndom"
+    }
+    move_json = json.dumps(move)
+    print(req)
+    return move_json
+
+
+
+@app.route("/start", methods=["GET"])
+def start_g(req):
+    return Snake.__repr__()
+
+
+@app.route("/move", methods=["GET"])
+def move_g(req):
+    return Snake.__repr__()
+
+
+app.run()
