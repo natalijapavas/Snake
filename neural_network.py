4bd0cd5ce384575fc6cac78a7778495ab6bd41ff
Natalija <natalijapavasovic@gmail.com>
2019-12-04 23:10:37 +0100

    dodatak

--- neural_network.py
+++ neural_network.py
@@ -1,6 +1,10 @@
 import numpy as np
-def sigmoid(X):
-    return 1/(1+np.exp(-x))
+import self as self
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
 
 class SnakeGame:
     def __init__(self, snake, last_move, food, eaten):
@@ -9,7 +13,7 @@ class SnakeGame:
         self.food = food
         self.eaten = eaten
 
-    def update_snake:
+    def update_snake(self, move):
         self.last_move = move
         head = self.snake[-1]
 
@@ -18,11 +22,23 @@ class SnakeGame:
         else:
             self.snake.pop(0)
 
-        if self last_move == 'r':
-            self.snake_append((head[0]+1,head[1]))
+        if self.last_move == 'r':
+            self.snake_append((head[0] + 1, head[1]))
         elif self.last_move == 'l':
-            self.snake_append((head[0]-1,head[1]))
+            self.snake_append((head[0] - 1, head[1]))
         elif self.last_move == 'u':
-            self.snake_append((head[0],head[1]-1))
+            self.snake_append((head[0], head[1] - 1))
         elif self.last_move == 'd':
-            self.snake_append((head[0],head[1]+1))
+            self.snake_append((head[0], head[1] + 1))
+        if head in self.snake[:-1]:
+            raise Exception('ur bad')
+
+    def move(nns, i=0):
+        if i == len(nns) - 1:
+            k = nns[i].feed_forward(sg.make_tset())
+            if k.index(max(k)) == 0:
+                sg.update_snake('r')
+            elif k.index(max(k)) == 1:
+                sg.update_snake('l')
+            elif k.index(max(k)) == 2:
+                sg.update_snake('d')
